85f8c5403536d2ad2c83f60fc3f1f2ef1bb3625f
snailking <39387494+snailking@users.noreply.github.com>
2018-12-09 17:35:15 +0100

    Add files via upload

--- fundscript.js
+++ fundscript.js
@@ -30,13 +30,11 @@ window.addEventListener("load", function() {
 // Get the modal
 var modal = document.getElementById("modal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementById("close");
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+// When the user clicks on button, close the modal
+function CloseModal() {
     modal.style.display = "none";
 }
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
